77271576a33932ece1e0ab79f11e811af6d40668
Leona <Leona@192.168.1.120>
2022-04-25 18:06:16 -0300

    15.6 - Mexendo com a Faixa de tempo usando o ChronoUnit

--- modulos_datas/src/modulos_datas/DatasEmJava.java
+++ modulos_datas/src/modulos_datas/DatasEmJava.java
@@ -2,22 +2,15 @@ package modulos_datas;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
 public class DatasEmJava {
 
 	public static void main(String[] args) throws ParseException {
-	Calendar calendar = Calendar.getInstance(); /*Pega a Data Atual*/
-		calendar.setTime(new SimpleDateFormat("dd-MM-yyyy").parse("10-04-2022"));
-		
-		calendar.add(Calendar.DAY_OF_MONTH, 5);/*Data de hoje mais 5 dias*/
-		System.out.println("Somando o dia do mês: " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
-		
-		calendar.add(Calendar.MONTH, 1);
-		System.out.println("Somando Mês : " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
-		
-		calendar.add(Calendar.YEAR, 1);
-		System.out.println("Somando ano : " + new SimpleDateFormat("dd-MM-yyyy").format(calendar.getTime()));
-	}
-}
+	
+	long dias = ChronoUnit.DAYS.between(LocalDate.parse("2022-02-01"),LocalDate.now());
+	System.out.println("Possui "+ dias + " dias entre a faixa de data");
+}}
